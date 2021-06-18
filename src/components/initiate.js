09f47357965dcf8f6b003408744941de874cc53a
westshae <shaewest02@gmail.com>
2021-06-18 23:00:25 +1200

    Creates components based on state, added input validation for number of courses

--- src/components/initiate.js
+++ src/components/initiate.js
@@ -13,28 +13,36 @@ const Input = styled.input`
     display:flexbox;
 `
 
-
-
 class initiate extends Component{
     constructor(props){
         super(props);
         this.state = {
-            firstInput: "",
+            numCourses: 1,
         }
     }
 
-    handleInputChange(e) {
-        const value = e.target.value;
+    handleNumCoursesChange(e) {
+        //Checks if the value isn't a string, if it isn't changes value to 1
+        let value = e.target.value;
+        if(!Boolean(value)){value = 1;}
+        if(isNaN(value)){value = 1;}
+
+        //Checks if the values fit the correct range, if not changes value
+        let valueInt = parseInt(value);
+        if(valueInt > 10){valueInt = 10;}    
+        if(valueInt <= 0){valueInt = 1;}
+
         this.setState({
-          firstInput: value
+          numCourses: valueInt
         })
     }
 
     render(){
         return(
             <Container>
-                <InputLabel>{this.state.firstInput}</InputLabel>
-                <Input type="text" onChange={this.handleInputChange.bind(this)} value={this.state.firstInput}/>
+                <InputLabel>Number of courses</InputLabel>
+                <Input type="number" onChange={this.handleNumCoursesChange.bind(this)} value={this.state.numCourses}/>
+                {Array(this.state.numCourses).fill(<p>Test</p>)}
             </Container>
         )
     }
