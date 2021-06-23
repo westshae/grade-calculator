ac56126870639987b1526dbf1db1d690873a1323
westshae <shaewest02@gmail.com>
2021-06-23 19:27:17 +1200

    changed if statements in paper to switch, rather than set value, it just doesn't update it.

--- src/components/paper.js
+++ src/components/paper.js
@@ -21,35 +21,53 @@ const Paper = () =>{
     const [total, setTotal] = useState(1);
 
     const handleGradePercent = e => {
-        //Checks if the value isn't a string, if it isn't changes value to 1
+        //Checks that the value passed in is a number, and checked that it isn't empty.
         let value = e.target.value;
-        if(!Boolean(value)){value = 1;}
-        if(isNaN(value)){value = 1;}
+        let valueInt = 0;
+        switch(value){
+            case (!Boolean(value)):
+                break;
+            case (isNaN(value)):
+                break;
+            default:
+                valueInt = parseInt(value)
+        }
 
-        //Checks if the values fit the correct range, if not changes value
-        let valueInt = parseInt(value);
-        if(valueInt > 15){valueInt = 15;}
-        if(valueInt <= 0){valueInt = 1;}
+        switch(valueInt){
+            case (valueInt > 100):
+                break;
+            case (valueInt < 0):
+                break;
+        }
 
         setGradePercent(valueInt)
     }
 
     const handlePercentOfGrade = e => {
-        //Checks if the value isn't a string, if it isn't changes value to 1
+        //Checks that the value passed in is a number, and checked that it isn't empty.
         let value = e.target.value;
-        if(!Boolean(value)){value = 1;}
-        if(isNaN(value)){value = 1;}
+        let valueInt = 0;
+        switch(value){
+            case (!Boolean(value)):
+                break;
+            case (isNaN(value)):
+                break;
+            default:
+                valueInt = parseInt(value)
+        }
 
-        //Checks if the values fit the correct range, if not changes value
-        let valueInt = parseInt(value);
-        if(valueInt > 15){valueInt = 15;}
-        if(valueInt <= 0){valueInt = 1;}
+        switch(valueInt){
+            case (valueInt > 100):
+                break;
+            case (valueInt < 0):
+                break;
+        }
 
         setPercentOfGrade(valueInt)
     }
 
     useEffect(()=>{
-        setTotal(percentOfGrade*gradePercent)
+        setTotal((percentOfGrade*.1)*(gradePercent*0.1))
     }, [percentOfGrade, gradePercent])
 
         return(
@@ -61,7 +79,7 @@ const Paper = () =>{
 
                 <Label>Percent of grade</Label>
                 <Input type="number" onChange={handlePercentOfGrade} defaultValue={1} value={percentOfGrade}/>
-                <p>Total: {total}</p>
+                <p>Total: {total}%</p>
                 <br/>
             </Container>
     )
